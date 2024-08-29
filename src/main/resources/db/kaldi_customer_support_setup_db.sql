48e61ee0ea762bdf56a01a9c6f650cc490257ea4
merela <mereladavid@gmail.com>
2024-08-29 22:22:02 +0200

    Updated database.
    -Added sender_type to the Message table to determining sender's type.
    
    -Added role to the Users and Supports tables to determining their roles.

--- src/main/resources/db/kaldi_customer_support_setup_db.sql
+++ src/main/resources/db/kaldi_customer_support_setup_db.sql
@@ -2,24 +2,24 @@ CREATE DATABASE IF NOT EXISTS kaldi_customer_support;
 
 use kaldi_customer_support;
 
-CREATE USER IF NOT EXISTS 'kaldi_admin'@'localhost' IDENTIFIED BY '+C7HZ+9z*yv%EfFM161';
-GRANT ALL PRIVILEGES ON kaldi_customer_support.* TO 'kaldi_admin'@'localhost';
-FLUSH PRIVILEGES;
-
-# TABLE CREATION
+#CREATE USER IF NOT EXISTS 'kaldi_admin'@'localhost' IDENTIFIED BY '+C7HZ+9z*yv%EfFM161';
+#GRANT ALL PRIVILEGES ON kaldi_customer_support.* TO 'kaldi_admin'@'localhost';
+#FLUSH PRIVILEGES;
 
 CREATE TABLE IF NOT EXISTS Users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
     user_password VARCHAR(255) NOT NULL,
-    username VARCHAR(255) NOT NULL UNIQUE
+    username VARCHAR(255) NOT NULL UNIQUE,
+    role VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS Support (
     id INT AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
     support_password VARCHAR(255) NOT NULL,
-    username VARCHAR(255) NOT NULL UNIQUE
+    username VARCHAR(255) NOT NULL UNIQUE,
+    role VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS Room (
@@ -34,13 +34,13 @@ CREATE TABLE IF NOT EXISTS Chatroom (
     support_id INT,
     FOREIGN KEY (user_id) REFERENCES Users(id),
     FOREIGN KEY (room_id) REFERENCES Room(id),
-    FOREIGN KEY (support_id) REFERENCES Support(id),
-    UNIQUE(user_id, room_id) 
+    UNIQUE(user_id, room_id)
 );
 
 CREATE TABLE IF NOT EXISTS Message (
     id INT AUTO_INCREMENT PRIMARY KEY,
     chatroom_id INT NOT NULL,
+    sender_type ENUM('USER', 'SUPPORT') NOT NULL,
     message VARCHAR(255) NOT NULL,
     timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (chatroom_id) REFERENCES Chatroom(id)
@@ -52,9 +52,9 @@ INSERT INTO Room (room_name) VALUES
 ('services'),
 ('talk');
 
-INSERT INTO Users (email,user_password,username) VALUES
-('testuser@gmail.com',"testpassword","testuser");
+INSERT INTO Users (email,user_password,username,role) VALUES
+('testuser@gmail.com',"$2a$10$B/XXZlcyE/a5dxv9v4i2l.Qi6xne4zupTHoRn4k5p4wo5z2NsymiG","testuser","user");
 
-INSERT INTO Support (email,support_password,username) VALUES
-('support@gmail.com',"supportpassword","support");
+INSERT INTO Support (email,support_password,username,role) VALUES
+('support@gmail.com',"$2a$10$yGbu4DLrfvck3XITb3kYuuFnKRmnyDmmSlif5TfwvkVDeKHg2L/6y","support","support");
 
