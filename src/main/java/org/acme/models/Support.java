fa6f714448156494204582d4a6e76b54c5d88215
merela <mereladavid@gmail.com>
2024-08-29 21:28:07 +0200

    Add role variable to Support class to represent role of the support user.
    Role variable will be used to authenticate http request to the REST API endpoint.

--- src/main/java/org/acme/models/Support.java
+++ src/main/java/org/acme/models/Support.java
@@ -1,9 +1,10 @@
 package org.acme.models;
-
 public class Support {
     private int id;
     private String email;
     private String username;
+
+    private String role;
 
     public Support() {
     }
@@ -12,6 +13,12 @@ public class Support {
         this.id = id;
         this.email = email;
         this.username = username;
+    }
+
+    public Support(String email, String username, String role) {
+        this.email = email;
+        this.username = username;
+        this.role = role;
     }
 
     public int getId() {
@@ -36,5 +43,13 @@ public class Support {
 
     public void setUsername(String username) {
         this.username = username;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
     }
 }
